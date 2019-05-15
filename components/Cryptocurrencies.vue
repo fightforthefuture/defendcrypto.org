e7abf2caee2f445d9a7c006d91c87a743b37e583
shoshi <shoshiroberts@gmail.com>
2019-05-15 15:32:57 -0400

    Customized crypto dropdown updates

--- components/Cryptocurrencies.vue
+++ components/Cryptocurrencies.vue
@@ -1,23 +1,105 @@
 <i18n src="~/locales/components/Cryptocurrencies.yml"></i18n>
 
 <style lang="scss">
+$mutliselect-height: 68px;
+
+// Multiselect > Chosen Selection area
 .multiselect,
 .multiselect__tags {
-  min-height: 50px;
+  min-height: $mutliselect-height;
   line-height: $base-line-height;
-  font-size: $base-font-size;
+  font-size: $m-font-size-4;
+
+  @include respond-to(lrg) {
+    font-size: $font-size-4;
+  }
 }
+.multiselect__tags {
+  padding: $gutter*1.8 $gutter*6 0 $gutter;
+  background: $grey-light-color;
+  border: 1px solid $grey-color;
+  color: $white;
+
+  @include respond-to(lrg) {
+    padding-top: $gutter*1.5;
+  }
+}
+.multiselect__tags .multiselect__placeholder {
+  padding-top: 0;
+  color: $white;
+}
+.multiselect--active .multiselect__placeholder {
+  display: inline;
+}
+
+// Multiselect > Chosen Selection area > Text containers
+.multiselect__input,
+.multiselect__single {
+  background: transparent;
+  font-size: $m-font-size-4;
+  text-overflow: clip;
+  white-space: nowrap;
+
+  @include respond-to(lrg) {
+    font-size: $font-size-4;
+  }
+}
+.multiselect__single .coin-logo {
+  margin-top: 2px;
+
+  @include respond-to(lrg) {
+    margin-top: 4px;
+  }
+}
+
+// Multselect > Drop down arrow
 .multiselect__select {
-  height: 48px;
+  width: 55px;
+  height: $mutliselect-height - 2;
+  padding: $gutter $gutter*2;
+  border-left: 1px solid $grey-color;
 }
+.multiselect--active .multiselect__select {
+  transform: rotateZ(0deg);
+}
+.multiselect__select:before {
+  display: block;
+  content: '';
+  width: 15px;
+  height: 9px;
+  top: 50%;
+  transform: translateY(-50%) rotateZ(0deg);
+  margin: 0 auto;
+  background: transparent url('~assets/images/arrow-down.svg') center center no-repeat;
+  border: none;
+  transition: transform 0.2s ease;
+}
+.multiselect--active .multiselect__select:before {
+  transform: translateY(-50%) rotateZ(180deg);
+}
+
+// Multi Select > Containers with Coin Logo
 .multiselect__single,
 .multiselect__option {
   overflow: hidden;
   line-height: $base-line-height;
-  font-size: $base-font-size;
 }
+
+// Multi Select > Drop down options container
 .multiselect__content-wrapper {
   height: 150px;
+  border-color: $grey-color;
+}
+
+// Multi Select > Drop down options container > Options
+.multiselect__option {
+  font-size: $base-font-size;
+}
+.multiselect__option.multiselect__option--highlight {
+  background: $grey-light-color;
+}
+.multiselect__option--selected.multiselect__option--highlight {
+  background: $warn-color;
 }
 </style>
 
@@ -34,8 +116,7 @@
       :placeholder="$t('choose_currency')"
       label="name"
       track-by="name"
-      :preselect-first="false"
-      id="example">
+      :preselect-first="false">
       <template slot="singleLabel" slot-scope="props">
         <div class="coin-logo">
           <img :src="props.option.logo" :alt="props.option.name">
