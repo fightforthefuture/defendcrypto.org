16b4a8dc6ab150437bd96cc9048f612233255774
shoshi <shoshiroberts@gmail.com>
2019-05-16 10:15:21 -0400

    Add link to why section

--- pages/index.vue
+++ pages/index.vue
@@ -90,7 +90,7 @@
         <div class="wrapper">
           <div class="row">
             <div class="sml-c12 lrg-c8 grid-center">
-              <h4>{{ $t('why_description') }}</h4>
+              <div v-html="$t('why_html')"></div>
             </div> <!-- .c -->
           </div> <!-- .row -->
         </div> <!-- .wrapper -->
